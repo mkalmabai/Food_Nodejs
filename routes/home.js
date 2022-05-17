ff2ae3e76597f16646063a1ecc5afcea66f03469
Margulan <akudajkul7@gmail.com>
2022-05-18 00:53:31 +0600

    make it better

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
-
-router
-    .route("/")
-    .get((req, res) =>
-        res.render("home")
-    );
-
-module.exports = router;
+// const express = require("express");
+// const path = require("path");
+// const router = express.Router();
+//
+// router
+//     .route("/")
+//     .get((req, res) =>
+//         res.render("home")
+//     );
+//
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,42 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT||3000
 
-const express = require("express");
-const app = express();
-const path = require("path");
-const logger = require('morgan');
-//git add .const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
-app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({extended:false}));
-//app.use(cookieParser());
-app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
+})
+// app.get('/home', (req, res) => {
+//     res.sendFile(__dirname + "/views/home.html");
+// })
 
-app.set('views', path.join(__dirname,'views'));
-app.set("view engine", "ejs");
-
-
-
-const PORT = process.env.POST||3000
-const homeRouter = require(path.join(__dirname, "routes", "home"))
-// const menuRouter = require('./routes/menu')
-// const searchmealRouter = require('./routes/searchmeal')
-// const orderRouter = require('./routes/order')
-
-
-app.use('/',homeRouter);
-app.use('/home',homeRouter);
-// app.use('/menu',menuRouter);
-// app.use('/searchmeal',searchmealRouter);
-// app.use('/order',orderRouter);
-
-
-
-
-
-
-
-
-app.use(express.static(path.join(__dirname, "public")));
-app.listen(PORT, () =>{
-    console.log(`App listening at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
 });
