27f88fac50214b535af752e67ddadd1fd55538cd
Margulan <akudajkul7@gmail.com>
2022-05-24 01:52:22 +0600

    make it better

--- config/databsconfig.js
+++ config/databsconfig.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url:'mongodb+srv://ass4:margulan2003@cluster0.9k7gq.mongodb.net/?retryWrites=true&w=majority'
+    url:'mongodb+srv://admin-Zhandos:Qwerty12345@cluster0.9k7gq.mongodb.net/?retryWrites=true&w=majority'
 }
